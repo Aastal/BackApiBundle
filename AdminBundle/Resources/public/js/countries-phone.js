c082d16c339de41d91961df2a64ccfd4425d65cf
AVaast <alex.vaast@gmail.com>
2017-02-10 12:16:06 +0100

    Gestion des pays pour le champ tel

---
+++ AdminBundle/Resources/public/js/countries-phone.js
@@ -0,0 +1,16 @@
+var flagContainer = $('.flag-container');
+var countryList = $('.country-list');
+var country = $('.country');
+var selectedFlag = $('.selected-flag');
+
+flagContainer.on('click', function () {
+    countryList.toggleClass('hide');
+});
+
+country.on('click', function () {
+    var dialCode = $(this).find('.dial-code').text();
+    var flag = $(this).find('.iti-flag').attr('class').split(' ').slice(-1);
+
+    selectedFlag.find('.iti-flag').removeClass().addClass('iti-flag ' + flag);
+    selectedFlag.find('.selected-dial-code').text(dialCode);
+});
