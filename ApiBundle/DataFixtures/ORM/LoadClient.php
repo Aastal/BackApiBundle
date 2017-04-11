9570d233a08c5dd37ea9ebc045223770468b94cb
AVaast <alex.vaast@gmail.com>
2017-04-11 17:30:04 +0200

    Ajout fixtures client bundle

--- ApiBundle/DataFixtures/ORM/LoadClient.php
+++ ApiBundle/DataFixtures/ORM/LoadClient.php
@@ -27,32 +27,49 @@ class LoadClient extends AbstractFixture implements OrderedFixtureInterface , Co
     public function load(ObjectManager $manager)
     {
         $name = $this->container->getParameter('geoks_admin.app_name');
-        $redirect = $this->container->getParameter('base_url');
-        $grants = ["password", "refresh_token", $this->container->getParameter('facebook_grant_type')];
-        $root = $this->container->getParameter('kernel.root_dir');
+        $client = $manager->getRepository('GeoksApiBundle:Client')->findOneBy(['name' => $name]);
 
-        $parameters = [$root . '/config/parameters.yml', $root . '/config/parameters.yml.dist', $root . '/config/parameters.aws.yml'];
+        if (!$client) {
 
-        $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
+            $redirect = $this->container->getParameter('base_url');
+            $grants = ["password", "refresh_token", $this->container->getParameter('facebook_grant_type')];
+            $root = $this->container->getParameter('kernel.root_dir');
 
-        $client = $clientManager->createClient();
+            $parameters = [$root . '/config/parameters.yml', $root . '/config/parameters.yml.dist', $root . '/config/parameters.aws.yml'];
 
-        $client->setName($name);
-        $client->setRedirectUris([$redirect]);
-        $client->setAllowedGrantTypes($grants);
+            $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
 
-        $manager->persist($client);
-        //$manager->flush();
+            $client = $clientManager->createClient();
 
-        $dumper = new Dumper();
-        $yaml = new Parser();
+            $client->setName($name);
+            $client->setRedirectUris([$redirect]);
+            $client->setAllowedGrantTypes($grants);
 
-        foreach ($parameters as $parameter) {
-            $yaml->parse(file_get_contents($parameter));
+            $manager->persist($client);
+            $manager->flush();
 
+            $dumper = new Dumper();
+            $yaml = new Parser();
+
+            foreach ($parameters as $parameter) {
+                $array = $yaml->parse(file_get_contents($parameter));
+
+                foreach ($array as &$a) {
+                    foreach ($a as $key => &$value) {
+                        if ($key == "api_client_id") {
+                            $value = $client->getPublicId();
+                        }
+
+                        if ($key == "api_client_secret") {
+                            $value = $client->getSecret();
+                        }
+                    }
+                }
+
+                $data = $dumper->dump($array, 4);
+                file_put_contents($parameter, $data);
+            }
         }
-
-        var_dump($yaml);
     }
 
     public function getOrder()
