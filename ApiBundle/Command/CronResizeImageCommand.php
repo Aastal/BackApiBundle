57f4d294f820d277dfa84517f042b3de0af1f976
AVaast <alex.vaast@gmail.com>
2017-07-07 12:24:43 +0200

    Rework resize image to service

--- ApiBundle/Command/CronResizeImageCommand.php
+++ ApiBundle/Command/CronResizeImageCommand.php
@@ -11,7 +11,6 @@ use Gaufrette\Adapter\AwsS3;
 use Liip\ImagineBundle\Imagine\Cache\Resolver\AmazonS3Resolver;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -26,80 +25,6 @@ class CronResizeImageCommand extends CronTaskCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // If thumb config is defined
-        if ($sizes = $this->getContainer()->getParameter('liip_imagine.filter_sets')["resize_thumb"]["filters"]) {
-
-            $stringManager = $this->getContainer()->get('geoks.utils.string_manager');
-            $imgUtil = $this->getContainer()->get('geoks.utils.image');
-            $system = new Filesystem();
-            $root = $this->getContainer()->get('kernel')->getRootDir();
-
-            $em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-            $metas = $em->getMetadataFactory()->getAllMetadata();
-            $reader = new AnnotationReader();
-
-            $vichMappings = $this->getContainer()->getParameter('vich_uploader.mappings');
-
-            $fsaws = $this->getContainer()->get('geoks.api.aws')->getS3Instance();
-
-            // Search in all the entities
-            foreach ($metas as $meta) {
-
-                $output->writeln($meta->getName());
-
-                /** @var ClassMetadata $meta */
-                $classReflection = $meta->getReflectionClass();
-
-                // Check if the entity can upload a file
-                if ($reader->getClassAnnotation($classReflection, "Vich\\UploaderBundle\\Mapping\\Annotation\\Uploadable")) {
-
-                    $find = false;
-                    foreach ($classReflection->getProperties() as $reflectionProperty) {
-
-                        if (!$find && $annotation = $reader->getPropertyAnnotation($reflectionProperty, "Geoks\\ApiBundle\\Annotation\\FilePath")) {
-
-                            $find = true;
-                            $path = $annotation->path;
-                            $target = $stringManager->getEndOfString("/", $vichMappings[$path]["uri_prefix"]);
-
-                            $files = $fsaws->listKeys($target);
-
-                            foreach ($files as $file) {
-
-                                // Resize the file, depend of the project config
-                                foreach ($sizes as $key => $size) {
-                                    $filename = $stringManager->getEndOfString("_", $file);
-
-                                    if (!$fsaws->has($target . "/thumb_" . $key . "_" . $stringManager->getEndOfString("/", $filename))) {
-
-                                        $system->mkdir($root . "/../web/assets/$target");
-                                        $newFile = $fsaws->get($file);
-                                        $system->dumpFile($root . "/../web/assets/" . $newFile->getName(), $newFile->getContent(), 0777);
-
-                                        $newFile = new File($root . "/../web/assets/" . $newFile->getName());
-                                        $system->copy($newFile, $root . "/../web/assets/$target" . "/thumb_" . $key . "_" . $newFile->getFilename());
-                                        $newFile = new File($root . "/../web/assets/$target" . "/thumb_" . $key . "_" . $newFile->getFilename());
-
-                                        $resize = $imgUtil->resizeImage($newFile, $size["size"][0], $size["size"][1]);
-
-                                        if ($resize) {
-                                            $fsaws->write($target . "/" . $newFile->getFilename(), file_get_contents($newFile));
-
-                                            $output->writeln($newFile->getFilename());
-                                            $system->remove($root . "/../web/assets/$target/" . $stringManager->getEndOfString("/", $file));
-                                            $system->remove($root . "/../web/assets/$target/" . $newFile->getFilename());
-                                        } else {
-                                            $output->write("error image : " . $newFile->getFilename());
-                                        }
-                                    } else {
-                                        $output->write("=");
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        $this->getContainer()->get('geoks.uploader')->resizeUpload();
     }
 }
